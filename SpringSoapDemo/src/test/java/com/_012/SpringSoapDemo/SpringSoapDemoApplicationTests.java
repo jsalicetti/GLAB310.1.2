acee3bc8505a850840dd9fe409bfcb3e0099cb9d
jsalicetti <jessicasalicetti@yahoo.com>
2024-09-17 10:46:33 -0400

    complete

---
+++ SpringSoapDemo/src/test/java/com/_012/SpringSoapDemo/SpringSoapDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com._012.SpringSoapDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoapDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
